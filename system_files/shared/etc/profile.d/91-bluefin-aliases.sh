9ce28e9d55bbb7eb62b62c07e27ef707047ab649
msmithnexus <msmithnexus@tiscalinet.it>
2026-06-03 23:58:35 +0200

    reset

--- system_files/shared/etc/profile.d/91-bluefin-aliases.sh
+++
@@ -1,5 +0,0 @@
-# Bluefin convenience aliases
-# ramalama shorthand - https://github.com/containers/ramalama
-if command -v ramalama &>/dev/null; then
-    alias rl='ramalama'
-fi
--- system_files/shared/usr/share/ublue-os/user-setup.hooks.d/99-privileged.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-echo "Running all privileged units"
-
-pkexec /usr/bin/ublue-privileged-setup
