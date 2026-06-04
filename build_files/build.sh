dc873993d22edb6f9779592d3b4f7008351be100
msmithnexus <msmithnexus@tiscalinet.it>
2026-06-04 14:39:36 +0200

    bazzite-niri-dms-t1

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,78 +1,99 @@
 #!/bin/bash
-
 set -ouex pipefail
 
-## DNF5 Speedup
-sed -i '/^\[main\]/a max_parallel_downloads=10' /etc/dnf/dnf.conf
+IMAGE_NAME="${IMAGE_NAME:-bazzite-dms-niri}"
+IMAGE_VARIANT="${IMAGE_VARIANT:-}"
 
-## System apps
-#dnf -y install libvirt virt-manager qemu-kvm flatpak-builder wlr-randr iotop sysstat lxqt-openssh-askpass lxpolkit parallel
+if [ -z "$IMAGE_VARIANT" ]; then
+  FULL_NAME="$IMAGE_NAME"
+else
+  FULL_NAME="${IMAGE_NAME}-${IMAGE_VARIANT}"
+fi
 
-# User apps
-#dnf -y install nautilus kitty mpv gnome-terminal gnome-system-monitor
+IMAGE_REF="ostree-image-signed:docker://ghcr.io/irunatbullets/${FULL_NAME}"
 
-# OBS and fully-featured ffmpeg with nonfree components from rpm fusion
-dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-dnf -y install ffmpeg x264-libs obs-studio obs-studio-plugin-x264 --allowerasing
+dnf5 install -y tmux
 
-# Nautilus open any terminal extension
-#curl -Lo /etc/yum.repos.d/nautilus-open-any-terminal.repo \
-#  https://copr.fedorainfracloud.org/coprs/monkeygold/nautilus-open-any-terminal/repo/fedora-$(rpm -E %fedora)/monkeygold-nautilus-open-any-terminal-fedora-$(rpm -E %fedora).repo
-#dnf install -y nautilus-open-any-terminal
-#glib-compile-schemas /usr/share/glib-2.0/schemas
-#gsettings set com.github.stunkymonkey.nautilus-open-any-terminal terminal kitty
+dnf5 -y copr enable avengemedia/dms
+dnf5 -y copr enable avengemedia/danklinux
 
+dnf5 -y install \
+    acl \
+    breakpad \
+    cliphist \
+    cava \
+    danksearch \
+    dgop \
+    dms \
+    dms-greeter \
+    ghostty \
+    material-symbols-fonts \
+    matugen \
+    niri \
+    qt6-qt5compat \
+    qt6-qtimageformats \
+    qt6-qtmultimedia \
+    qt6-qtsvg \
+    quickshell \
+    wl-clipboard
 
-# Install Niri 
-dnf -y install niri 
+dnf5 -y copr disable avengemedia/dms
+dnf5 -y copr disable avengemedia/danklinux
 
-# # Install Noctalia shell
-# curl -fsSL https://github.com/terrapkg/subatomic-repos/raw/main/terra.repo -o /etc/yum.repos.d/terra.repo
-# dnf -y install terra-release
-# dnf -y install noctalia-shell 
-# # ABILITARE LE NOTIFICHE: systemctl --user enable --now swaync.service
+dnf5 -y install rust cargo @development-tools dbus-devel xcb-util-cursor-devel clang git
 
-# Install Dank Linux shell
-sudo curl --output-dir "/etc/yum.repos.d/" \
-  --remote-name "https://copr.fedorainfracloud.org/coprs/avengemedia/dms/repo/fedora-$(rpm -E %fedora)/avengemedia-dms-fedora-$(rpm -E %fedora).repo"
-dnf -y install quickshell dms greetd dms-greeter --allowerasing 
-#
-# Install greetd login manager with dank configuration (still needs some work)
-mkdir -p /etc/greetd/
-cat > /etc/greetd/config.toml << EOF
-[terminal]
-vt = 1
-[default_session]
-user = "greeter"
-command = "dms-greeter --command niri"
-EOF
-rm -f /etc/systemd/system/display-manager.service
-ln -s /usr/lib/systemd/system/greetd.service /etc/systemd/system/display-manager.service
-systemctl enable --force greetd.service
+(
+    export CARGO_HOME=/tmp/cargo
+    export RUSTUP_HOME=/tmp/rustup
+    export CARGO_INSTALL_ROOT=/usr
 
-mkdir -p /etc/skel/.config/systemd/user/graphical-session.target.wants
-ln -s /usr/lib/systemd/user/dms.service /etc/skel/.config/systemd/user/graphical-session.target.wants/
-mkdir -p /etc/skel/.config/niri/
-cp -rf /ctx/dot_config/niri/config.kdl /etc/skel/.config/niri/
+    cargo install bluetui
 
-# DEV packages
-# cargo evtest git input-remapper libevdev-devel libinput-utils python3-devel
+    cd /tmp
+    git clone https://github.com/Supreeeme/xwayland-satellite.git
+    cd xwayland-satellite
+    git checkout a879e5e
 
-# dnf -y install bitwarden-cli 
+    cargo build --release
+    install -Dm755 target/release/xwayland-satellite /usr/bin/xwayland-satellite
+)
 
-#### Enable podman
+rm -rf /tmp/cargo /tmp/rustup /tmp/xwayland-satellite
+dnf5 -y remove rust cargo @development-tools dbus-devel xcb-util-cursor-devel clang git
 
 systemctl enable podman.socket
+systemctl --global enable dms
 
-# Disable Origami tips
+systemctl disable gdm
+systemctl enable greetd
 
-sudo mv /etc/profile.d/origami-aliases.sh /etc/profile.d/origami-aliases.sh.bak
+mkdir -p /var/cache/dms-greeter
 
-# Remove COSMIC shell and waybar
-dnf -y remove cosmic-comp cosmic-initial-setup cosmic-settings cosmic-settings-daemon cosmic-store  waybar
+tee /usr/lib/sysusers.d/greeter.conf <<'EOF'
+g greeter 767
+u greeter 767 "greetd greeter" /var/lib/greeter
+EOF
 
-## CLEAN UP
-# Clean up dnf cache to reduce image size
-dnf5 -y clean all
-rm -rf /run/dnf /run/selinux-policy
-rm -rf /var/lib/dnf
+mkdir -p /var/lib/greeter
+chown 767:767 /var/lib/greeter
+
+mkdir -p /usr/lib/systemd/user/graphical-session.target.wants
+
+ln -s /usr/lib/systemd/user/dms-niri-config.service \
+    /usr/lib/systemd/user/graphical-session.target.wants/dms-niri-config.service
+
+ln -s /usr/lib/systemd/user/dsearch.service \
+    /usr/lib/systemd/user/graphical-session.target.wants/dsearch.service
+
+jq \
+  --arg name "$FULL_NAME" \
+  --arg ref "$IMAGE_REF" \
+  --arg tag "${IMAGE_VARIANT:-latest}" \
+  '
+  .["image-name"]=$name |
+  .["image-ref"]=$ref |
+  .["image-tag"]=$tag
+  ' \
+  /usr/share/ublue-os/image-info.json \
+  > /tmp/image-info.json && mv /tmp/image-info.json /usr/share/ublue-os/image-info.json
+
