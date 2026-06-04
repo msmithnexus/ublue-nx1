da67b3a1159f083e6fd3a06f0e381b503b51215f
msmithnexus <msmithnexus@tiscalinet.it>
2026-06-04 21:18:38 +0200

    bazzite-niri-t1

--- build_files/build.sh
+++ build_files/build.sh
@@ -7,18 +7,30 @@ set -ouex pipefail
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# remove kde plasma
+dnf5 -y remove plasma-workspace plasma-* kde-*
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
+# setup niri
+dnf5 -y install					\
+	niri						\
+	alacritty					\
+	gdm							\
+	xdg-desktop-portal-gtk		\
+	xdg-desktop-portal-gnome	\
+	gnome-keyring				\
+	nautilus					\
+	mako						\
+	fuzzel						\
+	waybar						\
+	swayidle					\
+	swaylock					\
+	polkit-kde					\
+	xwayland-satellite			\
+	swaybg
 
 systemctl enable podman.socket
+systemctl --global add-wants niri.service mako.service
+systemctl --global add-wants niri.service swayidle.service
+systemctl --global add-wants niri.service plasma-polkit-agent.service
